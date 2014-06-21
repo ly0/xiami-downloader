dc9bcac86602b02137d4f83ee894606458ec5f50
Yangjie Li <latyas@gmail.com>
2014-06-21 17:00:29 +0800

    cover, PEP8
    1. Node of album's cover changed in returned XML data
    2. PEP8 passed

--- xiami.py
+++ xiami.py
@@ -5,41 +5,68 @@ Created on Fri Nov  1 20:33:08 2013
 @author: latyas
 """
 
-# NOT FINISHED YET
-
-import xmltodict
-import requests 
 import sys
-import urllib2
+try:
+    import xmltodict
+except:
+    print('missing library: xmltodict')
+    sys.exit(0)
+try:
+    import requests
+except:
+    print('missing library: requests')
+    sys.exit(0)
+
+try:
+    import urllib2
+except:
+    import urllib.parse as urllib2
 import os
-from BeautifulSoup import BeautifulSoup
+
+
+try:
+    from BeautifulSoup import BeautifulSoup
+except:
+    print('missing library: xmltodict')
+    sys.exit(0)
+
 import json
 import getopt
 
-username = 'USERNAME'
-password = 'PASSWORD'
+username = 'test5@yopmail.com'
+password = '19920330'
 axel_opts = '-n5'
 
+
 def text_validate(text):
-    return text.replace('\'','').replace('\\','').replace('/','')
+    return text.replace('\'', '').replace('\\', '').replace('/', '')
+
+
 def set_320k(s):
     url = 'http://www.xiami.com/vip/myvip'
-    header = {'user-agent':'Mozilla/5.0',
-              'Referer':'http://img.xiami.com/static/swf/seiya/player.swf?v=1394535902294'}
-    ret = s.get(url,headers=header).text
+    header = {'user-agent': 'Mozilla/5.0',
+              'Referer': 'http://img.xiami.com/static/swf/seiya/'
+                         'player.swf?v=1394535902294'}
+
+    ret = s.get(url, headers=header).text
     bs = BeautifulSoup(ret)
-    user_id = bs.find('input',attrs={'id':'user_id'}).get('value')
-    data = {'user_id':user_id,
-            'tone_type':'1',
-            '_xiamitoken':s.cookies.get('_xiamitoken')
+    user_id = bs.find('input', attrs={'id': 'user_id'}).get('value')
+    data = {'user_id': user_id,
+            'tone_type': '1',
+            '_xiamitoken': s.cookies.get('_xiamitoken')
             }
-    ret = s.post('http://www.xiami.com/vip/update-tone',data=data,headers=header)
+
+    ret = s.post('http://www.xiami.com/vip/update-tone', data=data,
+                 headers=header)
+
     ret = json.loads(ret.text)
     if ret['info'] == 'success':
-        print 'Qualities of the songs have been set to 320kbps.'
+        print('Qualities of the songs have been set to 320kbps.')
     else:
-        print 'You are not a VIP, songs will be downloaded with normal quality.'
- 
+        print('You are not a VIP, songs will be downloaded'
+              ' with normal quality.')
+
+
 def xiami(s):
     start = s.find('h')
     row = int(s[0:start])
@@ -47,107 +74,144 @@ def xiami(s):
     column = length / row
     output = ''
     real_s = list(s[1:])
-    
     sucks = []
-    suck = length % row # = 0 -> good! if not , sucks!
-    for i in range(1,suck+1):
+    suck = length % row  # = 0 -> good! if not , sucks!
+    for i in range(1, suck+1):
         sucks.append(real_s[i*(column)])
         real_s[i*(column)] = 'sucks'
         real_s.remove('sucks')
     for i in range(column):
-        output += ''.join(real_s[i:][slice(0,length,column)])
+        output += ''.join(real_s[i:][slice(0, length, column)])
     output += ''.join(sucks)
-    return urllib2.unquote(output).replace('^','0')
+    return urllib2.unquote(output).replace('^', '0')
 
 
 def usage():
-    print '''Usage: %s --type=album/songlist/single [--remove] [--320k] [--onefolder] listid
-                 --remove: deletes files if existed.
-                 --320k: downloads 320kbps first (VIP needed) 
-                 --onefolder: all musics will be downloaded into one folder "songlist_listid", if type is songlist''' % (sys.argv[0])
-    
+    print('''Usage: %s --type=album/songlist/single \
+          [--remove] [--320k] [--onefolder] id_list \
+                 --remove: delete files if already existed \
+                 --320k: download for 320kbps primarily (VIP needed) \
+                 --onefolder: all musics will be downloaded into one folder \
+                 "songlist_listid", if type is songlist''' % (sys.argv[0]))
+
 
 def exception():
     usage()
     sys.exit(1)
 
+
 def login(s):
-    header = {'user-agent':'Mozilla/5.0'}
+    header = {'user-agent': 'Mozilla/5.0'}
     login_url = 'https://login.xiami.com/member/login'
-    data = {'email':username,
-            'password':password,
-            'done':'http://www.xiami.com/account',
-            'submit':'登 录'
-    }
-    ret = s.post(login_url,data=data,headers=header)
+    data = {'email': username,
+            'password': password,
+            'done': 'http://www.xiami.com/account',
+            'submit': '登 录'}
+
+    s.post(login_url, data=data, headers=header)
 
 
-def download(s,album_type,id):
-    global arg_remove,arg_onefolder
+def download(s, album_type, id):
+    global arg_remove, arg_onefolder
     if album_type != 'single':
-        foo = s.get('http://www.xiami.com/song/playlist/id/%s/type/%s' % (id,album_type),headers={'user-agent':'Mozilla/5.0'}).text
+        foo = s.get('http://www.xiami.com/song/playlist/id/'
+                    '%s/type/%s' % (id, album_type),
+                    headers={'user-agent': 'Mozilla/5.0'}).text
     else:
-        foo = s.get('http://www.xiami.com/song/playlist/id/%s' % (id),headers={'user-agent':'Mozilla/5.0'}).text
-    data = xmltodict.parse(foo)['playlist']['trackList']['track'] 
+        foo = s.get('http://www.xiami.com/song/playlist/id/%s' % (id),
+                    headers={'user-agent': 'Mozilla/5.0'}).text
+
+    data = xmltodict.parse(foo)['playlist']['trackList']['track']
+
     delete_all = arg_remove
-    
-    if arg_onefolder == True:
+
+    if arg_onefolder is True:
         if not os.path.exists('songlist_%s' % id):
-            print 'Creating folder'
+            print('Creating folder')
             os.system('mkdir \'%s\'' % ('songlist_%s' % id))
     if album_type != 'single':
         for i in data:
-            if arg_onefolder != True:
+            if arg_onefolder is not True:
                 folder = text_validate(i['album_name'])
                 if not os.path.exists(folder):
-                    print 'Creating folder'
+                    print('Creating folder')
                     os.system('mkdir \'%s\'' % folder)
                 if not os.path.exists('%s/cover.jpg' % (folder)):
-                    print 'Downloading cover ...'
-                    os.system('curl \'%s\' > \'%s/cover.jpg\'' % (i['pic'].replace('_1',''), folder))          
+                    print('Downloading cover ...')
+                    os.system('curl \'%s\' > \'%s/cover.jpg\'' % (
+                              i['album_pic'].replace('_1', ''),
+                              folder)
+                              )
             else:
                 folder = 'songlist_%s' % id
 
             if not hq:
                 url = xiami(i['location'])
             else:
-                url = xiami(json.loads(req.get('http://www.xiami.com/song/gethqsong/sid/' + i['song_id'],headers=header).text)['location'])
-            print 'Downloading',i['title']
-            if os.path.exists('%s/%s.mp3' % (folder,text_validate(i['title']))):
+                url = xiami(json.loads(req.get(
+                            'http://www.xiami.com/song/gethqsong/sid/'
+                            + i['song_id'], headers=header).text)['location'])
+
+            print('Downloading', i['title'])
+
+            if os.path.exists('%s/%s.mp3' % (folder,
+                              text_validate(i['title']))):
 
                 if not delete_all:
-                    foofoo = raw_input('%s existed, delete?(yes/ALL for files existed/others to skip)' % i['title'])
+                    foofoo = raw_input('%s existed, '
+                                       'do you want to delete it?'
+                                       '(yes/ALL for files existed/'
+                                       'others for skipping)'
+                                       % i['title'])
+
                     if foofoo == 'ALL':
-                       delete_all = True
+                        delete_all = True
+
                     if foofoo != 'yes' and foofoo != 'ALL':
-                        print '********************** skipped *************************'
+                        print('skipped')
                         continue
 
-                os.system('rm \'%s/%s.mp3\'' % (folder,text_validate(i['title'])))
-            os.system('axel -n5 --user-agent="Mozilla/5.0" %s -o \'%s\'' % (url, '%s/%s.mp3' %(folder,text_validate(i['title']))))
+                os.system('rm \'%s/%s.mp3\''
+                          % (folder, text_validate(i['title'])))
+
+            os.system('axel -n5 --user-agent="Mozilla/5.0" %s -o \'%s\''
+                      % (url, '%s/%s.mp3' % (folder,
+                                             text_validate(i['title']))))
     else:
         if not hq:
             url = xiami(data['location'])
         else:
-            url = xiami(json.loads(req.get('http://www.xiami.com/song/gethqsong/sid/' + data['song_id'],headers=header).text)['location'])
+            url = xiami(json.loads(
+                        req.get('http://www.xiami.com/song/gethqsong/sid/'
+                                + data['song_id'],
+                                headers=header).text)['location'])
+
         folder = 'singles'
 
         if not os.path.exists(folder):
-            print 'Creating [singles] folder'
+            print('Creating [singles] folder')
             os.system('mkdir \'%s\'' % folder)
-            
-        print 'Downloading',data['title']
-        if os.path.exists('%s/%s.mp3' % (folder,text_validate(data['title']))):
+
+        print('Downloading', data['title'])
+        if os.path.exists('%s/%s.mp3' % (folder, text_validate(data['title']))):
 
             if not delete_all:
-                foofoo = raw_input('%s existed, delete?(yes/ALL for files existed/others to skip)' % data['title'])
-                if foofoo == 'ALL':
-                   delete_all = True
-                if foofoo != 'yes' and foofoo != 'ALL':
-                    print '********************** skipped *************************'
+                foofoo = raw_input('%s existed, delete?'
+                                   '(yes/ALL for files existed/others to skip)'
+                                   % data['title'])
 
-            os.system('rm \'%s/%s.mp3\'' % (folder,text_validate(data['title'])))
-        os.system('axel -n5 --user-agent="Mozilla/5.0" %s -o \'%s\'' % (url, '%s/%s.mp3' %(folder,text_validate(data['title']))))
+                if foofoo == 'ALL':
+                    delete_all = True
+
+                if foofoo != 'yes' and foofoo != 'ALL':
+                    print('skipped')
+
+            os.system('rm \'%s/%s.mp3\''
+                      % (folder, text_validate(data['title'])))
+
+        os.system('axel -n5 --user-agent="Mozilla/5.0" %s -o \'%s\''
+                  % (url, '%s/%s.mp3'
+                          % (folder, text_validate(data['title']))))
 
 if __name__ == '__main__':
     reload(sys)
@@ -156,15 +220,18 @@ if __name__ == '__main__':
     arg_remove = False
     arg_lists = []
     arg_type = ''
-    hq = False #high quality switch
+    hq = False  # high quality switch
     arg_onefolder = False
 
-    opts,args = getopt.getopt(sys.argv[1:],'h',['remove','type=','help','320k','onefolder'])
+    opts, args = getopt.getopt(sys.argv[1:],
+                               'h',
+                               ['remove', 'type=', 'help', '320k', 'onefolder'])
+
     foo = [i[0] for i in opts]
     if '--type' not in foo or args == []:
         exception()
 
-    for o,v in opts:
+    for o, v in opts:
         if o == '--remove':
             arg_remove = True
         elif o == '--type' and v != '':
@@ -178,19 +245,19 @@ if __name__ == '__main__':
                 exception()
         elif o == '--320k':
             hq = True
-        elif o == '--onefolder' and ('--type','songlist') in opts:
+        elif o == '--onefolder' and ('--type', 'songlist') in opts:
             arg_onefolder = True
         elif o == '--help':
             exception()
         else:
-            print 'unrecognized argument',o
+            print('unrecognized argument', o)
             exception()
     # song list
     arg_lists.extend(args)
 
+    header = {'user-agent': 'Mozilla/5.0',
+			  'Referer': 'http://img.xiami.com/static/swf/seiya/player.swf?v=1394535902294'}
 
-    header = {'user-agent':'Mozilla/5.0',
-			  'Referer':'http://img.xiami.com/static/swf/seiya/player.swf?v=1394535902294'}
     album = sys.argv[1]
     list_type = sys.argv[2]
 
@@ -198,9 +265,8 @@ if __name__ == '__main__':
 
     login(req)
     if hq:
-        set_320k(req) # set quality to 320kbps
-
+        set_320k(req)  # set quality to 320kbps
 
     for i in arg_lists:
-        print 'id:%s' % i
-        download(req,arg_type,i)
+        print('id:%s' % i)
+        download(req, arg_type, i)
